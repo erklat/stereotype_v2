2fec2b630cf615a9c3b459403a1351fcc8b39e0f
Davor Matičević <9886276+erklat@users.noreply.github.com>
2024-09-08 07:18:55 +0200

    feature(products): adds product table and initial product (#8)
    Co-authored-by: DavorM <davor.maticevic@myzone.com>

--- src/utils/ProductsManager/product.api.ts
+++ src/utils/ProductsManager/product.api.ts
@@ -1,8 +1,8 @@
-import restClient from "@/api/restClient";
+import { localRestClient } from "@/api/restClient";
 import { DefinedInitialDataOptions } from "@tanstack/react-query";
 
 export const getProductsAPI = async (params?: any) => {
-  const res = await restClient.get(`/products`, {
+  const res = await localRestClient.get(`/products`, {
     params: { ...params, limit: 0 },
   });
   const data = await res?.data;
